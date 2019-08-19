c516ed7897a932e025f52818c1e8ce879415eb55
Miguel Lopez Ariza <lopezarizamiguel@gmail.com>
2019-08-18 21:36:48 -0500

    Updates

---
+++ precache-manifest.23be22af263ba42ad48376ff914933a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bae2335ebbe580b491176e5004da5a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4494350097b1c97c68b",
+    "url": "./static/css/main.ea18eb9f.chunk.css"
+  },
+  {
+    "revision": "bf106dfcbb42f1635a2e",
+    "url": "./static/js/2.9e8fcc00.chunk.js"
+  },
+  {
+    "revision": "e4494350097b1c97c68b",
+    "url": "./static/js/main.7050db30.chunk.js"
+  },
+  {
+    "revision": "8c92a71a868b1824293a",
+    "url": "./static/js/runtime~main.c14eaa64.js"
+  }
+]);
